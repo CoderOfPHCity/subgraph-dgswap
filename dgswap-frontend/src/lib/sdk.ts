d6dd2ff7f1f000396a6119a1bab82f7fe4e319b6
coderofphcity <agantemboy@gmail.com>
2025-02-27 04:44:54 +0100

    all

--- dgswap-frontend/src/lib/sdk.ts
+++
@@ -1,4 +0,0 @@
-import { DragonSwapSubgraphSDK } from "@PaulElisha/subgraph-dgswap";
-
-const ENDPOINT = `https://gateway.thegraph.com/api/${import.meta.env.VITE_GRAPH_API_KEY}/subgraphs/id/2QEZxd4f115iTkW38MFmL6KZsq9uAYmCaFF2KLrZeSjK`;
-export const sdk = new DragonSwapSubgraphSDK(ENDPOINT);
--- dgswap-frontend/src/utils/format.ts
+++
@@ -1,3 +0,0 @@
-export const formatUSD = (value: string | number): string => {
-    return Number(value).toLocaleString("en-US", { style: "currency", currency: "USD" });
-  };
